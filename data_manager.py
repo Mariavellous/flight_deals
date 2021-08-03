f12a0e95959112dd6183f9b4e3539e61749ae04a
Maria Melanie Alcaide <mariamelanie@Marias-MacBook-Pro.local>
2021-08-02 21:38:50 -0700

    Use the Sheety API to GET all the flight data in that sheet and print it out.

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,27 @@
+
+import requests
+FLIGHT_DATA_ENDPOINT = "https://api.sheety.co/91fd50510c8320842d8851c13cbed29d/flightDeals/prices"
+AUTHORIZATION_HEADERS = {
+    "Authorization": "Bearer hackalert"
+  }
+
+
+# This class is responsible for talking to the Google Sheet.
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    # 2. Use the Sheety API to GET all the data in that sheet and print it out.
+    def destination_data(self):
+        sheet_response = requests.get(FLIGHT_DATA_ENDPOINT, headers=AUTHORIZATION_HEADERS)
+        print(sheet_response.status_code)
+        data = sheet_response.json()
+        return data
+
+
+data_manager = DataManager()
+data_manager.destination_data()
+print(data_manager.destination_data())
+
+
+
+
+
