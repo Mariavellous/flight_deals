5a2c334bc260eb5b9473dd3e148acb5785bdf8a7
Mariavellous <mm.alcaide38@gmail.com>
2021-08-07 15:26:17 -0700

    Update the city to its proper IATA code

--- flight_search.py
+++ flight_search.py
@@ -1,5 +1,5 @@
 # This class is responsible for talking to the Flight Search API.
-
+import requests
 
 class FlightSearch:
   def __init__(self):
@@ -7,6 +7,14 @@ class FlightSearch:
 
   # For now, the FlightSearch class can respond with "TESTING" instead of IATA code
   def get_iata_code(self, city):
-    code = "TESTING2"
-    return code
+    kiwi_url = "https://tequila-api.kiwi.com/locations/query"
+    headers = {"apikey": "Jb4o_zhT1mu6JEaY8Z8ZWDgVADWHjV1W"}
+    city_params = {
+      "term": city,
+      "location_types": "airport",
+    }
+    response = requests.get(url=kiwi_url, params=city_params, headers=headers)
+    data = response.json()
+    iata_code = data["locations"][0]["id"]
+    return iata_code
 
